e6aa3f21a5b8cdb5122cb34b53c94fbc2c6656d1
1Buhle <buhlesonwabile@gmail.com>
2025-08-29 10:00:37 +0200

    Impliment the art section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
 import { Analytics } from "@vercel/analytics/react"
+import Art from "./components/Art.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -15,6 +16,7 @@ const App = () => {
             <Hero />
             <Cocktails />
             <About />
+            <Art />
 
         </main>
     )
